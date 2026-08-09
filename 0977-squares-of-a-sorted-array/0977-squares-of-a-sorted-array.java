b46b70931e8f8c49db1635b93af8a1f55b6f3b04
Rhytm Jain <rj27122005@gmail.com>
2026-08-09 22:23:30 +0530

    Time: 1 ms (100%), Space: 46.9 MB (97.74%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int [] res = new int[nums.length];
+        int l=0,r = nums.length-1;
+        for(int i=nums.length-1; i>=0;i--){
+            if(Math.abs(nums[l])<Math.abs(nums[r])){
+                res[i] = nums[r] * nums[r];
+                r--;
+            }
+            else{
+                res[i]=nums[l]* nums[l];
+                l++;
+            }
+        }
+        return res;
+    }
+}
